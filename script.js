11ec78ba305bd2f63043e5bbbcaa373381b57dec
Thiago Waldeck <thiagowsvianna@gmail.com>
2024-04-18 21:23:20 -0300

    criação de navegaçao de desktop, remoão da classe header e início responsivilidade

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 const botaoAbrir = document.querySelector(".header-mobile > button")
-const botaofechar = document.querySelector(".header-mobile mav button")
-
+const botaoFechar = document.querySelector(".header-mobile nav button")
+const botaoAbrir = document.querySelector("header > button")
+const botaoFechar = document.querySelector("header nav button")
 
 const body = document.querySelector("body")
 const nav = document.querySelector(".header-mobile nav")
+const nav = document.querySelector("header .mobile")
 
-console.log
-
-botaoAbrir.addEventListener("click",abrirmenu)
-botaofechar.addEventListener("click, fecharmenu")
-
-
-function abrirMenu () {
-       body.classList.add("escurecer")
-       nav.classList.add("abrir")
+botaoAbrir.addEventListener('click', abrirMenu)
+botaoFechar.addEventListener('click', fecharMenu)
+function abrirMenu() {
+    body.classList.add("escurecer")
+    nav.classList.add("Abrir")
 }
-
-function fecharMenu () {
+function fecharMenu() {
     body.classList.remove("escurecer")
-    nav.classList.remove("abrir")
+    nav.classList.remove("Abrir")
 }
