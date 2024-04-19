e9d675aac1629b00c9d47de2e3e6add28df5f2d4
Thiago Waldeck <thiagowsvianna@gmail.com>
2024-04-18 21:47:37 -0300

    trabalho pronto

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-const botaoAbrir = document.querySelector(".header-mobile > button")
-const botaoFechar = document.querySelector(".header-mobile nav button")
 const botaoAbrir = document.querySelector("header > button")
 const botaoFechar = document.querySelector("header nav button")
 
 const body = document.querySelector("body")
-const nav = document.querySelector(".header-mobile nav")
 const nav = document.querySelector("header .mobile")
 
 botaoAbrir.addEventListener('click', abrirMenu)
 botaoFechar.addEventListener('click', fecharMenu)
+
 function abrirMenu() {
     body.classList.add("escurecer")
     nav.classList.add("Abrir")
 }
+
 function fecharMenu() {
     body.classList.remove("escurecer")
     nav.classList.remove("Abrir")
